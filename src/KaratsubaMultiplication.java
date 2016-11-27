6dfe5b34316c68853db17eb1f780ab160d3d8703
Abhishek Velayudham <avelay@terpmail.umd.edu>
2016-11-27 15:27:27 -0500

    First Commit

---
+++ src/KaratsubaMultiplication.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class KaratsubaMultiplication {
+
+	public static void main(String[] args) {
+		/*&= sc = new Scanner(System.in);
+		Integer num1 = sc.nextInt();
+		Integer num2 = sc.nextInt();*/
+		//karatsubaMultiplication(num1, num2);
+		System.out.println(karatsubaMultiplication(55, 55));
+	}
+	
+	private static int karatsubaMultiplication(Integer num1, Integer num2) { // not working for digits with odd number of digits
+		if (num1 < 10 || num2 < 10) return num1*num2;
+		
+		String num1Str = String.valueOf(num1);
+		String num2Str = String.valueOf(num2);
+		int padding = (int) Math.pow(10, Integer.max(num1Str.length(), num2Str.length()));
+		int padding2 = (int) Math.pow(10, Integer.max(num1Str.length()/2, num2Str.length()/2));
+		int a = Integer.parseInt(num1Str.substring(0, num1Str.length()/2));
+		int b = Integer.parseInt(num1Str.substring(num1Str.length()/2));
+		int c = Integer.parseInt(num2Str.substring(0, num2Str.length()/2));
+		int d = Integer.parseInt(num2Str.substring(num2Str.length()/2));
+		
+		int ac = karatsubaMultiplication(a, c);
+		int bd = karatsubaMultiplication(b, d);
+		int abcd = karatsubaMultiplication(a+b, c+d);
+		int all = abcd-bd-ac;
+		int answer = (ac*padding) + bd + (all*padding2);
+		return answer;
+		}	
+}
